f9cebf14f9d57908fb978543c52d178ebac74e7b
Landris18 <landry.manankoraisina.rasendranirina@esti.mg>
2020-10-24 12:11:47 +0300

    Création de navigation drawer et tab beta

--- gfs.py
+++ gfs.py
@@ -5,14 +5,24 @@ Config.set('graphics', 'height', '580')
 
 from kivymd.app import MDApp
 from kivy.uix.screenmanager import Screen
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.floatlayout import FloatLayout
+from kivymd.uix.tab import MDTabsBase
+from kivymd.icon_definitions import md_icons
 from kivy.lang import Builder
+
+
+class Tab(FloatLayout, MDTabsBase):
+    '''Class implementing content for a tab.'''
+
+class ContentNavigationDrawer(BoxLayout):
+    pass
 
 class GFS(MDApp):
     def build(self):
         self.theme_cls.theme_style= "Light"
         self.theme_cls.primary_palette = "DeepOrange"
         self.theme_cls.primary_hue = "A700"
-        
-        return Builder.load_file('login.kv')
-                            
+        return Builder.load_file('navigation.kv')
+
 GFS().run()                
